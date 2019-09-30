c438c9c109196f17e4209810d4b95d61de3c9a0a
pankajgouraw <pgouraw@gmail.com>
2019-09-30 16:57:22 +0530

    initial commit

---
+++ data/data.js
@@ -0,0 +1,81 @@
+
+const background = 'background-image: radial-gradient(#3dcd47,#1a1a1a);';
+const header = 'Match the correct options';
+const headerColor = '#fff';
+
+
+const data = [
+	{
+		'listA' :[
+	                "next.png",
+	                "prev.png",
+	                "happy.png",
+	                "sad.png"
+		        ],
+
+		'listB' :[
+	                "next",
+	                "happy",
+	                "Prev",
+	                'sad' 
+                ],
+
+        'answer' :[
+	               {"from":"next.png","to":"Prev"},
+	               {"from":"prev.png","to":"next"},
+	               {"from":"happy.png","to":'happy'},
+	               {"from":"sad.png","to":"sad"}
+                 ]	
+	},
+
+	{
+		'listA' :[
+	                "next.png",
+	                "prev.png",
+	                "happy.png",
+	                "sad.png"
+		        ],
+
+		'listB' :[
+	                "next",
+	                "happy",
+	                "Prev",
+	                'sad' 
+                ],
+
+        'answer' :[
+	               {"from":"next.png","to":"Prev"},
+	               {"from":"prev.png","to":"next"},
+	               {"from":"happy.png","to":'happy'},
+	               {"from":"sad.png","to":"sad"}
+                 ]		
+	},
+
+	{
+				'listA' :[
+	                "next.png",
+	                "prev.png",
+	                "happy.png",
+	                "sad.png"
+		        ],
+
+		'listB' :[
+	                "next",
+	                "happy",
+	                "Prev",
+	                'sad' 
+                ],
+
+        'answer' :[
+	               {"from":"next.png","to":"Prev"},
+	               {"from":"prev.png","to":"next"},
+	               {"from":"happy.png","to":'happy'},
+	               {"from":"sad.png","to":"sad"}
+                 ]	
+	}
+	
+];
+
+
+
+
